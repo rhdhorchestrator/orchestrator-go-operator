45bf408075ce7cfffca30730f6bd30edc8de001d
Jennifer Ubah <cju.cipher@gmail.com>
2025-01-29 18:18:19 -0500

    orchestrator plugin to rc.7

--- internal/controller/rhdh/plugins.go
+++ internal/controller/rhdh/plugins.go
@@ -16,12 +16,12 @@ const NotificationsEmail string = "notificationsEmail"
 func getPlugins() map[string]Plugin {
 	return map[string]Plugin{
 		Orchestrator: {
-			Package:   "backstage-plugin-orchestrator@1.4.0-rc.3",
-			Integrity: "sha512-vGGd9hUmDriEMmP2TfzLVa3JSnfot2Blg+aftDnu/lEphsY1s2gdA4Z5lCxUk7aobcKE6JO/f0sIl2jyxZ7ktw==",
+			Package:   "backstage-plugin-orchestrator@1.4.0-rc.7.tgz",
+			Integrity: "sha512-Vclb+TIL8cEtf9G2nx0UJ+kMJnCGZuYG/Xcw0Otdo/fZGuynnoCaAZ6rHnt4PR6LerekHYWNUbzM3X+AVj5cwg==",
 		},
 		OrchestratorBackend: {
-			Package:   "backstage-plugin-orchestrator-backend-dynamic@1.4.0-rc.3",
-			Integrity: "sha512-tS5cJGwjzP9esdTZvUFjw0O7+w9gGBI/+VvJrtqYJBDGXcEAq9iYixGk67ddQVW5eeUM7Tk1WqJlNj282aAWww==",
+			Package:   "backstage-plugin-orchestrator-backend-dynamic@1.4.0-rc.7.tgz",
+			Integrity: "sha512-bxD0Au2V9BeUMcZBfNYrPSQ161vmZyKwm6Yik5keZZ09tenkc8fNjipwJsWVFQCDcAOOxdBAE0ibgHtddl3NKw==",
 		},
 	}
 

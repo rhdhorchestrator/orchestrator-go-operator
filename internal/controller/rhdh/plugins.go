8fd32214c9fddc3f119aeb27c0f2831e34a16f39
Jennifer Ubah <cju.cipher@gmail.com>
2025-02-20 05:56:12 -0500

    Updated plugins to 1.5 internal release version

--- internal/controller/rhdh/plugins.go
+++ internal/controller/rhdh/plugins.go
@@ -11,12 +11,12 @@ const OrchestratorBackend string = "orchestratorBackend"
 func getPlugins() map[string]Plugin {
 	return map[string]Plugin{
 		Orchestrator: {
-			Package:   "backstage-plugin-orchestrator@1.4.0-rc.7",
-			Integrity: "sha512-Vclb+TIL8cEtf9G2nx0UJ+kMJnCGZuYG/Xcw0Otdo/fZGuynnoCaAZ6rHnt4PR6LerekHYWNUbzM3X+AVj5cwg==",
+			Package:   "backstage-plugin-orchestrator-1.5.1-rc.1.tgz",
+			Integrity: "sha512-Pw1PfiGTLJzjRCINA3+s3zWy6L0WTmCZjOxa/iZlkpTN6ywAcmPD43wtWaFT5Hxhlr5NMa/2af291F4G1gAqcg==",
 		},
 		OrchestratorBackend: {
-			Package:   "backstage-plugin-orchestrator-backend-dynamic@1.4.0-rc.7",
-			Integrity: "sha512-bxD0Au2V9BeUMcZBfNYrPSQ161vmZyKwm6Yik5keZZ09tenkc8fNjipwJsWVFQCDcAOOxdBAE0ibgHtddl3NKw==",
+			Package:   "backstage-plugin-orchestrator-backend-dynamic-1.5.1-rc.1.tgz",
+			Integrity: "sha512-3YJYUFSaxNTQc5k40sfmVdibXFzbYEeiyBrGLOdcG6mB+Vdm90Hbv+isa6y3bbsK22juXbtAjsl39TtVGI1esg==",
 		},
 	}
 

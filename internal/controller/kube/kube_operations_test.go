5f591ad346a6126d52fd543007b344c80a5afe1a
Jennifer Ubah <cju.cipher@gmail.com>
2025-03-04 23:28:07 -0500

    Added unit test cases for subscription install

--- internal/controller/kube/kube_operations_test.go
+++ internal/controller/kube/kube_operations_test.go
@@ -18,6 +18,9 @@ package kube
 
 import (
 	"context"
+	operatorsv1 "github.com/operator-framework/api/pkg/operators/v1"
+	"github.com/operator-framework/api/pkg/operators/v1alpha1"
+	olmclientsetfake "github.com/operator-framework/operator-lifecycle-manager/pkg/api/client/clientset/versioned/fake"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -27,13 +30,16 @@ import (
 	"testing"
 )
 
+var (
+	orchestratorNamespaceName = "orchestrator-namespace"
+	orchestratorOperatorGroup = "orchestrator-operator-group"
+)
+
 func TestCheckNamespaceExist(t *testing.T) {
 	ctx := context.TODO()
 	// Create a fake client scheme
 	scheme := runtime.NewScheme()
 	utilruntime.Must(corev1.AddToScheme(scheme))
-
-	orchestratorNamespaceName := "orchestrator-namespace"
 
 	ns := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{Name: orchestratorNamespaceName},
@@ -57,16 +63,62 @@ func TestCreateNamespace(t *testing.T) {
 	scheme := runtime.NewScheme()
 	utilruntime.Must(corev1.AddToScheme(scheme))
 
-	orchestratorNamespaceName := "orchestrator-namespace"
 	ns := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: orchestratorNamespaceName, Labels: AddLabel()}}
 
 	// Test create namespace
-	fakeClientWithNS := fake.NewClientBuilder().WithScheme(scheme).Build()
-	err := CreateNamespace(ctx, fakeClientWithNS, orchestratorNamespaceName)
+	fakeClientWithoutNS := fake.NewClientBuilder().WithScheme(scheme).Build()
+	err := CreateNamespace(ctx, fakeClientWithoutNS, orchestratorNamespaceName)
 	assert.NoError(t, err, "Expected no error")
 
 	// Test create namespace with error
-	fakeClientWithoutNS := fake.NewClientBuilder().WithScheme(scheme).WithObjects(ns).Build()
-	err = CreateNamespace(ctx, fakeClientWithoutNS, orchestratorNamespaceName)
+	fakeClientWithNS := fake.NewClientBuilder().WithScheme(scheme).WithObjects(ns).Build()
+	err = CreateNamespace(ctx, fakeClientWithNS, orchestratorNamespaceName)
 	assert.Error(t, err, "Expected an error when namespace does not exist")
+}
+
+func TestInstallSubscriptionAndOperatorGroup(t *testing.T) {
+	ctx := context.TODO()
+	scheme := runtime.NewScheme()
+	utilruntime.Must(v1alpha1.AddToScheme(scheme))
+	utilruntime.Must(operatorsv1.AddToScheme(scheme))
+
+	subscriptionName := "orchestrator-subscription"
+	subscription := &v1alpha1.Subscription{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      subscriptionName,
+			Namespace: orchestratorNamespaceName,
+		},
+		Spec: &v1alpha1.SubscriptionSpec{
+			Channel:                "channel",
+			StartingCSV:            "starting-csv",
+			InstallPlanApproval:    v1alpha1.ApprovalManual,
+			CatalogSource:          CatalogSourceName,
+			CatalogSourceNamespace: CatalogSourceNamespace,
+		},
+	}
+
+	fakeClient := fake.NewClientBuilder().WithScheme(scheme).Build()
+
+	// Test subscription is created
+	fakeOLMClientSetWithoutSubscription := olmclientsetfake.NewSimpleClientset()
+	err := InstallSubscriptionAndOperatorGroup(
+		ctx, fakeClient,
+		fakeOLMClientSetWithoutSubscription,
+		orchestratorOperatorGroup,
+		subscription)
+	assert.NoError(t, err, "Expected no error")
+
+	// Test create subscription with error
+	fakeOLMClientSetWithSubscription := olmclientsetfake.NewSimpleClientset()
+	fakeOLMClientSetWithSubscription.OperatorsV1alpha1().
+		Subscriptions(orchestratorNamespaceName).
+		Create(ctx, subscription, metav1.CreateOptions{})
+
+	err = InstallSubscriptionAndOperatorGroup(
+		ctx,
+		fakeClient,
+		fakeOLMClientSetWithSubscription,
+		orchestratorOperatorGroup,
+		subscription)
+	assert.Error(t, err, "Expected error")
 }

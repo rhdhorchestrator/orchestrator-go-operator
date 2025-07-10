dc6aada576111cf08749cb1ff505c08c3297aef7
Jennifer Ubah <cju.cipher@gmail.com>
2025-07-10 12:58:10 -0400

    Update orchestrator plugins to v1.6.1-rc1

--- internal/controller/rhdh/configmap_ref.go
+++ internal/controller/rhdh/configmap_ref.go
@@ -6,6 +6,7 @@ const (
 	AppConfigRHDHCatalogName       = "app-config-rhdh-catalog"
 	AppConfigRHDHDynamicPluginName = "dynamic-plugins-rhdh"
 	NpmRegistry                    = "https://npm.registry.redhat.com"
-	Scope                          = "@redhat"
-	CatalogBranch                  = "v1.6.x"
+	//Scope                          = "@redhat" // for production
+	Scope         = "https://github.com/rhdhorchestrator/orchestrator-plugins-internal-release/releases/download/v1.6.1-rc.1"
+	CatalogBranch = "v1.6.x"
 )
